c051ea93d7456ae00d24cdacacc673340de4c1c3
Alvarohf <neoncommander@gmail.com>
2019-10-17 19:32:35 +0200

    Updated with new AT conf. code

--- examples/bluetooth_module/AT_mode/AT_mode.ino
+++ examples/bluetooth_module/AT_mode/AT_mode.ino
@@ -1,21 +1,27 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial BT1(10, 11); // RX | TX
-void setup()
-   { pinMode(8, OUTPUT);        // Al poner en HIGH forzaremos el modo AT
-     pinMode(9, OUTPUT);        // cuando se alimente de aqui
-     digitalWrite(9, HIGH);
-     delay (500) ;              // Espera antes de encender el modulo
-     Serial.begin(9600);
-     Serial.println("Levantando el modulo HC-06");
-     digitalWrite (8, HIGH);    //Enciende el modulo
-     Serial.println("Esperando comandos AT:");
-     BT1.begin(57600); 
-   }
+SoftwareSerial mySerial(10, 11); // RX, TX
 
-void loop()
-   {  if (BT1.available())
-            Serial.write(BT1.read());
-      if (Serial.available())
-            BT1.write(Serial.read());
-   }
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.println("Goodnight moon!");
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(38400);
+  mySerial.println("Hello, world?");
+}
+
+void loop() { // run over and over
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
+  }
+}
