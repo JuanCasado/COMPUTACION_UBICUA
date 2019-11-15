c36a9214062e8b81022ebbde1610cfe13045413d
Colibro <34507863+Colibro@users.noreply.github.com>
2019-11-15 14:00:43 +0100

    Add files via upload

--- Rasp_src/bluetooth_cama_constante.py
+++ Rasp_src/bluetooth_cama_constante.py
@@ -2,13 +2,16 @@
 import os
 import serial
 import time
+import json
+import requests
 
 conectado = 0
-
+url = "http://172.22.38.145:8080"
 
 while 1:
     if not conectado:
         try:
+            #Realizamos intento de conexión al arduino
             os.system('sudo rfcomm connect hci0 98:D3:32:31:B8:3C 1')
             bluetoothSerial= serial.Serial("/dev/rfcomm0", baudrate=115200)
             print("Bluetooth connected")
@@ -20,11 +23,30 @@ while 1:
     if conectado:
         try:
             print("Printeando")
+            #data = json.loads(bluetoothSerial.readline()))
+            #data = bluetoothSerial.readline()
             data = bluetoothSerial.readline()
+            data = data.decode("utf-8")
+            
             print(data)
+            data = json.loads(data)
+            
+            answer = "{"
+            for idx,key in enumerate(data):
+                value = data[key]
+                answer += key + ": {"
+                answer += "Valor:" + str(value) + ","
+                answer += "Timestamp: " + str(time.time())
+                answer += "}"
+                if idx != (len(data)-1):
+                    answer += ","
+            answer += "}"
+            
             time.sleep(1)
-                
+            
+            r = requests.post(url,answer)
+            print(r)
         except KeyboardInterrupt:
             print("Quiting...")
-        except:
-            conectado = 0
+        """except:
+            conectado = 0"""
