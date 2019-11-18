1aaf725b477a2f596b0b25cbf252eb6a7df82b48
JuanCasado <juan.casado@edu.uah.es>
2019-11-18 18:00:21 +0100

    body parser mongo

--- src/server/database_api/src/main.ts
+++ src/server/database_api/src/main.ts
@@ -2,11 +2,13 @@ import express from "express"
 import mongoose from 'mongoose'
 import bp from 'body-parser'
 const app = express()
+app.use(bp.json())
+app.use(bp.urlencoded({extended: true}))
 const port = 80
 
 app.get("/", (req, res) => {
-  console.log(req)
-  res.send(req)
+  console.log(req.body)
+  res.send(req.body)
 })
 
 app.listen(port)
