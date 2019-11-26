74dfb1d5e21bc5905ef67655e63f0d8356330fd5
unknown <neoncommander@gmail.com>
2019-11-26 21:08:32 +0100

    Updated presentation

--- presentation/index_htm_files/1.js
+++ presentation/index_htm_files/1.js
@@ -1,10 +1,24 @@
-window.local_cloudZoomtint_v10_0_data = [
+window.local_zaccordionc_0_data = [
 {
- alt: "image1",
- imagebig: "index_htm_files/255.jpg",
- imagesmall: "index_htm_files/256.jpg",
- loading: "index_htm_files/257.png",
- title: "image1",
- viewertext: "Zoom Viewer label"
+ auto: "1",
+ speed: "1",
+ theme: "0",
+ timeout: "4",
+ trigger: "1"
+},
+{
+ content: "Cama con control ergon\u00F3mico y de firme.",
+ heading: "SleepNumber 360",
+ image: "index_htm_files/354.jpg"
+},
+{
+ content: "Cama con control ergon\u00F3mico a tiempo real.",
+ heading: "ReST Performance",
+ image: "index_htm_files/355.jpg"
+},
+{
+ content: "Cama calefactable e inteligente. ",
+ heading: "EightSleep",
+ image: "index_htm_files/356.jpg"
 }
 ];
--- presentation/index_htm_files/2.js
+++ presentation/index_htm_files/2.js
@@ -1,32 +1,8 @@
-window.local_cycleCircle_0_data = [
+window.local_HSColChart_0_data = [
 {
- effect: "2",
- pause: "2",
- speed: "1"
-},
-{
- image: "index_htm_files/259.jpg",
- image1: "index_htm_files/260.png",
- link: "#"
-},
-{
- image: "index_htm_files/261.jpg",
- image1: "index_htm_files/262.png",
- link: "#"
-},
-{
- image: "index_htm_files/263.jpg",
- image1: "index_htm_files/264.png",
- link: "#"
-},
-{
- image: "index_htm_files/265.jpg",
- image1: "index_htm_files/266.png",
- link: "#"
-},
-{
- image: "index_htm_files/267.jpg",
- image1: "index_htm_files/268.png",
- link: "#"
+ csv: "Categories, Precio\u000AEightSleep, 895\u000ASleepNumber360, 500\u000AReST Perfomance,3800\u000ANosotros, 200",
+ style: "Dolares",
+ theme: "1",
+ title: "Comparaci\u00F3n de precios"
 }
 ];
