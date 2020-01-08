312b3ad358d43c338b0aee03961820af22d74dc3
JuanCasado <juan.casado@edu.uah.es>
2020-01-08 22:25:39 +0100

    presentation2

--- src/server/web/src/main.js
+++ src/server/web/src/main.js
@@ -4,7 +4,8 @@ const port = 80
 const app = express()
 
 app.get("/", (req, res) => {res.sendFile('./index.html', { root: '../web/public'});})
-app.use('/presentation', express.static('../web/public/presentation'));
+app.use('/presentation1', express.static('../web/public/presentation1'));
+app.use('/presentation2', express.static('../web/public/presentation2'));
 app.use('/img', express.static('../web/public/img'));
 app.use('/src', express.static('../web/public/src'));
 app.use('/styles', express.static('../web/public/styles'));
