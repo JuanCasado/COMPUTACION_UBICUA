195ba34939cd1763f8d3e90738e03e0e892df971
JuanCasado <juan.casado@edu.uah.es>
2020-01-08 13:10:55 +0000

    rasp bt

--- src/raspberry/main.py
+++ src/raspberry/main.py
@@ -1,5 +1,6 @@
 
-from WiredConnection import WiredConnection
+#from WiredConnection import WiredConnection
+from BluetoothConnection import BluetoothConnection
 from RestSender import RestSender, Recivers
 from RestReciver import RestReciver
 from JSONFormater import formatToJsonMap, DataType
@@ -7,7 +8,8 @@ from Sounds import Songs, playSong
 
 if __name__=='__main__':
 	device_name = 'SmartBed1'
-	serial_connection = WiredConnection()
+	#serial_connection = WiredConnection()
+	serial_connection = BluetoothConnection()
 	rest_sender = RestSender()
 	rest_reciver = RestReciver()
 	serial_connection.connect()
@@ -15,6 +17,7 @@ if __name__=='__main__':
 		rest_sender.add_user(device_name).text
 	while True:
 		raw_data = serial_connection.read()
+		print(raw_data)
 		formated_data = formatToJsonMap(raw_data, device_name)
 		if formated_data != None:
 			rest_sender.send(formated_data[DataType.FLEX.value], Recivers.FLEX.value)
