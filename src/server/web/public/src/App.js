cfecf2f2213cd021ffe29b73f452328a19ef421c
unknown <neoncommander@gmail.com>
2020-01-08 22:43:18 +0100

    Added button

--- src/server/web/public/src/App.js
+++ src/server/web/public/src/App.js
@@ -4,17 +4,22 @@ function App() {
 		<div className="centered App">
 			<div id="menu" className="start-menu">
 				<div>
-					<h1 className="main-title">Smart <b className="blue-title">Bed</b> </h1> 
+					<h1 className="main-title">Smart <b className="blue-title">Bed</b> </h1>
 					<p className="sub-title">Ubiquitous Computing at UAH</p>
 				</div>
-				<div className="dropdown-divider"></div>
+				<div className="text-center">
+					<div className="dropdown-divider"></div>
 					<div className="small-margin">
 						<ApplicationButton />
-				</div>
-				<div className="dropdown-divider"></div>
-				<div className="text-center">
+					</div>
+					<div className="dropdown-divider">
+					</div>
 					<div className="small-margin">
 						<PresentationButton />
+					</div>
+					<div className="dropdown-divider"></div>
+					<div className="small-margin">
+						<PresentationButton2 />
 					</div>
 					<div className="dropdown-divider"></div>
 					<div className="small-margin">
